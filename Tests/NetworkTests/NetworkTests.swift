df7b54014ededbb63c38853d90a6807461b68e1d
Miguel Olmedo <miguel.olmedo@globant.com>
2021-03-04 12:19:47 -0600

    [Test] Add Test for get all breeds and get random  image

--- Tests/NetworkTests/NetworkTests.swift
+++ Tests/NetworkTests/NetworkTests.swift
@@ -1,15 +1,52 @@
 import XCTest
+import RxSwift
 @testable import Network
 
 final class NetworkTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Network().text, "Hello, World!")
+    
+    let api = DogAPI()
+    var disposeBag: DisposeBag!
+    
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
     }
+    
 
+    func test_get_all_breeds() {
+        //Given
+        let expect = expectation(description: "Waiting for request...")
+        
+        //When
+        api.getAllBreeds()
+            .subscribe { _ in
+                //Then
+                expect.fulfill()
+            } onError: { (error) in
+                //Then
+                XCTFail("Error \(error)")
+            }.disposed(by: disposeBag)
+        waitForExpectations(timeout: 2)
+    }
+    
+    func test_get_ramdom_image() {
+        //Given
+        let expect = expectation(description: "Waiting for request...")
+        
+        //When
+        api.getRandomImage()
+            .subscribe { response in
+                //Then
+                debugPrint(response)
+                expect.fulfill()
+            } onError: { (error) in
+                //Then
+                XCTFail("Error \(error)")
+            }.disposed(by: disposeBag)
+        waitForExpectations(timeout: 2)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("test_get_all_breeds", test_get_all_breeds),
     ]
 }
